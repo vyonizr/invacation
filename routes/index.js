c7abab1895f48650723bb80f69be123bd49273c7
vyonizr <fitrahtur@gmail.com>
2019-03-27 17:34:55 +0700

    add cancel booking feature

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,30 @@
 const express = require("express")
 const router = express.Router()
 const bcrypt = require("bcryptjs")
-const { User } = require("../models")
+const { User, UserDestination, Destination } = require("../models")
 const isLoggedIn = require("../middlewares/isLoggedIn")
 const destinationRoute = require("./destinationRoute")
 const userRoute = require("./userRoute")
+const getCompleteDate = require("../helpers/getCompleteDate")
 
 router.get("/", (req, res) => {
-  console.log(req.session);
   let foundUser = null
   if (req.session.loggedInUser) { // Logged in
-    User.findByPk(req.session.loggedInUser.id)
-    .then(user => {
-      foundUser = user
+    User.findByPk(req.session.loggedInUser.id, {
+      include: [
+        {model: Destination}
+      ]
+    })
+    .then(foundUser => {
       res.render("pages", {
         title: "InVacation",
         foundUser,
+        getCompleteDate,
         session: req.session
       })
+    })
+    .catch(err => {
+      res.send(err)
     })
   }
   else { // not Login
@@ -27,6 +34,26 @@ router.get("/", (req, res) => {
       session: req.session
     })
   }
+})
+
+router.post("/cancel/:destinationId", (req, res) => {
+  UserDestination.findOne({
+    include: [{model: User}, {model: Destination}],
+    where: {
+      UserId: req.session.loggedInUser.id,
+      DestinationId: req.params.destinationId
+    }
+  })
+  .then(foundUserDestination => {
+    foundUserDestination.User.balance += foundUserDestination.Destination.price * 0.5
+    return Promise.all([foundUserDestination.User.save(), foundUserDestination.destroy()])
+  })
+  .then(() => {
+    res.redirect("/")
+  })
+  .catch(err => {
+    res.send(err)
+  })
 })
 
 router.use("/destinations", destinationRoute)
