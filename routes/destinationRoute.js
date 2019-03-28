631e31bdf7cb3bcf96139d8befc14fd801f3f240
vyonizr <fitrahtur@gmail.com>
2019-03-28 14:21:09 +0700

    Improved booking system

--- routes/destinationRoute.js
+++ routes/destinationRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { Destination, User, Picture } = require("../models")
+const { Destination, User, Picture, UserDestination } = require("../models")
 const isLoggedIn = require("../middlewares/isLoggedIn")
 const getCompleteDate = require("../helpers/getCompleteDate")
 
@@ -43,19 +43,48 @@ router.get("/", (req, res) => {
 
 router.get("/:destinationId", (req, res) => {
   let foundUser = null
+  let err = null
+  let booked = false
+  let success = null
+  if (req.query.err) {
+    err = req.query.err
+  }
+  else if (req.query.success) {
+    success = req.query.success
+  }
   if (req.session.loggedInUser) {
     User.findByPk(req.session.loggedInUser.id)
     .then(user => {
       foundUser = user
-      return Destination.findByPk(req.params.destinationId)
+    })
+    UserDestination.findOne({
+      where: {
+        UserId: req.session.loggedInUser.id,
+        DestinationId: req.params.destinationId
+      }
+    })
+    .then(foundUserDestination => {
+      // res.send(foundUserDestination)
+      if(foundUserDestination) {
+        booked = true
+      }
+      return Destination.findByPk(req.params.destinationId, {
+        include: [
+          {model: Picture}
+        ]
+      })
     })
     .then(foundDestination => {
+      // res.send(foundDestination)
       res.render("pages/destinations/destination-detail", {
         title: `Invacation | ${foundDestination.name}`,
         foundDestinationStringify: JSON.stringify(foundDestination),
         foundUser,
         foundDestination,
         getCompleteDate,
+        booked,
+        success,
+        err,
         session: req.session
       })
     })
@@ -74,6 +103,9 @@ router.get("/:destinationId", (req, res) => {
         foundUser,
         foundDestination,
         getCompleteDate,
+        booked,
+        success,
+        err,
         session: req.session
       })
     })
@@ -83,49 +115,30 @@ router.get("/:destinationId", (req, res) => {
   }
 })
 
-router.post("/:destinationId/book", isLoggedIn, (req, res) => {
-  let foundUser = null
-  if (req.session.loggedInUser) {
-    User.findByPk(req.session.loggedInUser.id)
-    .then(user => {
-      foundUser = user
-      return Destination.findByPk(req.params.destinationId)
-    })
-    .then(foundDestination => {
-      res.render("pages/destinations/destination-detail", {
-        title: `Invacation | ${foundDestination.name}`,
-        foundDestinationStringify: JSON.stringify(foundDestination),
-        foundUser,
-        foundDestination,
-        getCompleteDate,
-        session: req.session
+router.get("/:destinationId/book", isLoggedIn, (req, res) => {
+  let destination = {}
+  Destination.findByPk(req.params.destinationId)
+  .then(foundDestination => {
+    destination = foundDestination
+    return User.findByPk(req.session.loggedInUser.id)
+  })
+  .then(foundUser => {
+    if (foundUser.balance < destination.price) {
+      res.redirect(`/destinations/${destination.id}?err=` + "You have insufficient funds")
+    }
+    else {
+      return UserDestination.create({
+        UserId: req.session.loggedInUser.id,
+        DestinationId: req.params.destinationId
       })
-    })
-    .catch(err => {
-      res.send(err)
-    })
-  }
-  else {
-    Destination.findByPk(req.params.destinationId, {
-      include: [
-        {model: Picture}
-      ]
-    })
-    .then(foundDestination => {
-      // res.send(foundDestination)
-      res.render("pages/destinations/destination-detail", {
-        title: `Invacation | ${foundDestination.name}`,
-        foundDestinationStringify: JSON.stringify(foundDestination),
-        foundUser,
-        foundDestination,
-        getCompleteDate,
-        session: req.session
-      })
-    })
-    .catch(err => {
-      res.send(err)
-    })
-  }
+    }
+  })
+  .then(() => {
+    res.redirect(`/destinations/${req.params.destinationId}?success=` + "Book success!")
+  })
+  .catch(err => {
+    res.send(err)
+  })
 })
 
 module.exports = router
